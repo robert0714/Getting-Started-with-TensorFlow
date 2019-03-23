fd1bae9fc1c3fe69841005c2d64b98ce5f537468
Robert <robert0714@gmail.com>
2019-03-23 11:58:35 +0800

    Update KNN.py

--- Chapter 3/KNN.py
+++ Chapter 3/KNN.py
@@ -19,26 +19,23 @@ test_pixel_tensor = tf.placeholder\
 
 distance = tf.reduce_sum\
            (tf.abs\
-            (tf.add(train_pixel_tensor, \
-                    tf.neg(test_pixel_tensor))), \
-            reduction_indices=1)
+            (tf.add(train_pixel_tensor, tf.negative(test_pixel_tensor))), reduction_indices=1)
 
-pred = tf.arg_min(distance, 0)
+pred = tf.math.argmin(distance, 0)
 
 # Testing and algorithm evaluation
 
 accuracy = 0.
-init = tf.initialize_all_variables()
+init = tf.global_variables_initializer()
 with tf.Session() as sess:
     sess.run(init)
     for i in range(len(test_list_of_values)):
         nn_index = sess.run(pred,\
-		feed_dict={train_pixel_tensor:train_pixels,\
-		test_pixel_tensor:test_pixels[i,:]})
-        print "Test N° ", i,"Predicted Class: ", \
-		np.argmax(train_list_values[nn_index]),\
-		"True Class: ", np.argmax(test_list_of_values[i])
+        feed_dict={train_pixel_tensor:train_pixels,\
+        test_pixel_tensor:test_pixels[i,:]})
+        print ("Test NÂ° ", i,"Predicted Class: ",np.argmax(train_list_values[nn_index]),\
+        "True Class: ", np.argmax(test_list_of_values[i]))
         if np.argmax(train_list_values[nn_index])\
-		== np.argmax(test_list_of_values[i]):
+        == np.argmax(test_list_of_values[i]):
             accuracy += 1./len(test_pixels)
-    print "Result = ", accuracy
+    print ("Result = ", accuracy)
